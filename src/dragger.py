21001b91db44284c92799f28b50381c5d5473717
boxler135 <boxler135@gmail.com>
2024-08-04 00:15:55 +0800

    added methods for dragging class

---
+++ src/dragger.py
@@ -0,0 +1,45 @@
+#import lib
+import pygame
+
+#custom
+from settings import (
+    WIDTH,
+    HEIGHT,
+    COLS,
+    ROWS,
+    SQSIZE
+)
+
+class Dragger:
+
+    def __init__(self):
+        self.piece = None
+        self.dragging = False
+        self.mouseX = 0
+        self.mouseY = 0
+        self.initial_row = 0
+        self.initial_col = 0
+
+    def update_mouse(
+            self,
+            pos
+    ):
+        self.mouseX, self.mouseY = pos
+    
+    def save_initial(
+            self,
+            pos
+    ):
+        self.initial_row = pos[1] // SQSIZE
+        self.initial_col = pos[0] // SQSIZE
+    
+    def drag_piece(
+            self,
+            piece
+    ):
+        self.piece = piece
+        self.draggin = True
+    
+    def undrag_piece(self):
+        self.piece = None
+        self.dragging = False
